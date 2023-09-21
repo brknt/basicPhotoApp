6525784b12eff225b3d0dd2908c9a6c794e29588
brknt <brknt.gns@hotmail.com>
2023-09-21 20:46:21 +0300

    redirect update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,10 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Photo = require('../models/Photo.js');
+
+
+
+
 const createUser = async (req, res) => {
     try {
 
@@ -79,8 +83,8 @@ const createToken = (userId) => {
 const getDashboardPage = async (req, res) => {
     const photos = await Photo.find({ user: res.locals.user._id });
     const user = await User.findById({ _id: res.locals.user._id }).populate(
-        ['followers','followings']
-        );// TODO: populate eklenecek.
+        ['followers', 'followings']
+    );// TODO: populate eklenecek.
 
     res.render('dashboard', {
         page_name: 'dashboard',
@@ -113,18 +117,24 @@ const getAllUsers = async (req, res) => {
 const getUser = async (req, res) => {
     try {
         const user = await User.findById({ _id: req.params.id });
-        const infollowers = user.followers.some((follower)=>{
-            return follower.equals(res.locals.user._id);
-        });
+        console.log(user._id, "-----", res.locals.user._id);
 
-        const photos = await Photo.find({ user: user._id });
+        if (user._id.equals(res.locals.user._id)) {
+            res.redirect('/users/dashboard');
+        } else {
+            const infollowers = user.followers.some((follower) => {
+                return follower.equals(res.locals.user._id);
+            });
 
-        res.status(200).render('user', {
-            user,
-            photos,
-            page_name: 'users',
-            infollowers
-        });
+            const photos = await Photo.find({ user: user._id });
+
+            res.status(200).render('user', {
+                user,
+                photos,
+                page_name: 'users',
+                infollowers
+            });
+        }
 
     } catch (error) {
         res.status(500).json({
@@ -169,21 +179,21 @@ const unfollow = async (req, res) => {
     try {
 
         let user = await User.findByIdAndUpdate(
-            {_id:req.params.id},
+            { _id: req.params.id },
             {
-             $pull :{followers:res.locals.user._id}   
+                $pull: { followers: res.locals.user._id }
             },
-            {new:true}
+            { new: true }
         );
 
         user = await User.findByIdAndUpdate(
-            {_id:res.locals.user._id},
-            {$pull:{followings:req.params.id}},
-            {new:true}
+            { _id: res.locals.user._id },
+            { $pull: { followings: req.params.id } },
+            { new: true }
         );
         res.status(200).redirect(`/users/${req.params.id}`);
 
-        
+
 
     } catch (error) {
         res.status(500).json({
